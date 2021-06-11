bc2bd7f5c38c0f4dd1ceeecc98425f01b2d1b63b
sarthak2806 <sathak2806singhania@gmail.com>
2021-06-11 18:50:54 +0530

    Insta

---
+++ server/keys.js
@@ -0,0 +1,4 @@
+module.exports={
+    MONGOURI:"mongodb+srv://assemblage_sarthak:hdaxtWMWDW9ZrT35@assemblage.caiw0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    JWT_SECRET:"SKHd5KKJxbxfgfVkjvbdk"
+}
